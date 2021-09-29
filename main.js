479f6837d51627232b75d23d243bfb46ef800aba
JulianP911 <j.padilla@uniandes.edu.co>
2021-09-28 22:44:41 -0500

    Vamos por el buen camino

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const urlInfo = 'https://gist.githubusercontent.com/josejbocanegra/9a28c356416ba
 // Obtener la informacion correspondiente del restaurante
 fetch(urlInfo).then(res => res.json()).then(restaurantEjecution)
 
+let a = [];
 // Ejecucion de las funcionalidades
 function restaurantEjecution (arrayFood) {
     let listCategories = document.getElementById("listCategories");
@@ -17,7 +18,9 @@ function restaurantEjecution (arrayFood) {
         listCategories.appendChild(li);
     });
 
-    
+    let divProductsCards = document.getElementById("products");
+    let quantity = [];
+
     document.querySelectorAll(".nav-link").forEach(itemNav => {
         itemNav.addEventListener("click", (event) => {
             // Variable de la categoria
@@ -28,7 +31,6 @@ function restaurantEjecution (arrayFood) {
             titleCategory.textContent = category;
 
             let listCategory = arrayFood.find(elementFood => elementFood.name == category);
-            let divProductsCards = document.getElementById("products");
             let numCards = 0;
 
             while (divProductsCards.lastElementChild) {
@@ -58,8 +60,15 @@ function restaurantEjecution (arrayFood) {
                 let buttomAdd = document.createElement("a");
                 buttomAdd.className = "btn btn-dark btn-item";
                 buttomAdd.setAttribute("type", "button");
+                buttomAdd.setAttribute("id", "button-" + category + "-" + item.name)
                 buttomAdd.textContent = "Add to card";
-                buttomAdd.addEventListener("click", countItems)
+                buttomAdd.addEventListener("click", function() {
+                    countItems(item, quantity)
+                });
+                
+                buttomAdd.addEventListener("click", function () {
+                    addItems(item)
+                });
 
                 divCard.appendChild(imgCard);
             
@@ -73,13 +82,143 @@ function restaurantEjecution (arrayFood) {
                 numCards++;
             });
         });
-    });  
+    });
+    
+    let tableItems = document.getElementById("tableItems");
+    let optionsItems = document.getElementById("optionsItems");
+    let tableHead = false;
+    document.getElementById("carItems").addEventListener("click", function () {
+        tableItems.innerHTML = "";
+        optionsItems.innerHTML = "";
+        while (divProductsCards.lastElementChild) {
+            divProductsCards.removeChild(divProductsCards.lastElementChild);
+        }
+
+        let title = document.getElementById("titleCategory");
+        title.textContent = "Order detail"
+
+        let table = document.createElement("table");
+        table.className ="table table_stripped";
+        
+        if(tableHead == false)
+        {
+            let tHead = document.createElement("thead");
+            let trHead = document.createElement("tr");
+            let nameColumns = ["Item", "Qty.", "Description", "Unit Price", "Amount", "Modify"]
+
+            for (let i = 0; i < nameColumns.length; i++) {
+                let trHd = document.createElement("th");
+                trHd.textContent = nameColumns[i];
+                trHd.setAttribute("scope", "col")
+                trHead.appendChild(trHd);
+            }
+
+            tHead.appendChild(trHead);
+            table.appendChild(tHead);
+            tableHead = true;
+        }
+
+        let tBody = document.createElement("tbody");
+
+        let index = 0;
+        let total = 0;
+        quantity.forEach(element => {
+            let tr = document.createElement("tr");
+            let thIndex = document.createElement("th");
+            thIndex.setAttribute("scope", "col");
+            thIndex.textContent = index;
+            let tdQty = document.createElement("td");
+            tdQty.textContent = element.quantity;
+            let tdDescription = document.createElement("td");
+            tdDescription.textContent = element.food;
+            let tdUnitPrice = document.createElement("td");
+            tdUnitPrice.textContent = element.unitPrice;
+            let tdAmount = document.createElement("td");
+            tdAmount.textContent = element.amount;
+            let tdButtons = document.createElement("td")
+            let buttomAdd = document.createElement("a");
+            buttomAdd.className = "btn btn-dark btn-row";
+            buttomAdd.textContent = "+";
+            buttomAdd.addEventListener("click", function() {
+                tdQty.textContent = ++element.quantity;
+                console.log(element.quantity * element.unitPrice);
+            });
+            let buttomLess = document.createElement("a");
+            buttomLess.className = "btn btn-dark btn-row";
+            buttomLess.addEventListener("click", function() {
+                tdQty.textContent = --element.quantity;
+            });
+            buttomLess.textContent = "-";
+
+            tr.appendChild(thIndex);
+            tr.appendChild(tdQty);
+            tr.appendChild(tdDescription);
+            tr.appendChild(tdUnitPrice);
+            tr.appendChild(tdAmount)
+
+            tdButtons.appendChild(buttomAdd);
+            tdButtons.appendChild(buttomLess);
+
+            tr.appendChild(tdButtons)
+            tBody.appendChild(tr);
+
+            total += element.amount;
+        });
+
+        console.log(total)
+        let divRow = document.createElement("div");
+        divRow.className = "row"
+        let divSpan = document.createElement("div");
+        divSpan.className = "col"
+        let spanTotal = document.createElement("span");
+        spanTotal.textContent = "Total: $" + total;
+        spanTotal.setAttribute("id", "spanTotalItems");
+        divSpan.appendChild(spanTotal);
+        let divButtoms = document.createElement("div");
+        divButtoms.className = "col d-flex justify-content-end"
+        divButtoms.setAttribute("id", "divButtoms");
+        let buttomCancel = document.createElement("a");
+        buttomCancel.className = "btn btn-danger btn-order";
+        buttomCancel.textContent = "Cancel";
+        let buttomConfirm = document.createElement("a");
+        buttomConfirm.className = "btn btn-light btn-order";
+        buttomConfirm.textContent = "Confirm order";
+        divButtoms.appendChild(buttomCancel);
+        divButtoms.appendChild(buttomConfirm);
+
+        divRow.appendChild(divSpan);
+        divRow.appendChild(divButtoms);
+
+        console.log(divRow);
+        
+        table.appendChild(tBody)
+        tableItems.appendChild(table)
+        optionsItems.appendChild(divRow);
+    });
 }
 
 let numItems = 0;
 let itemCar = document.getElementById("itemsAdd")
 
-function countItems() {
-    itemCar.textContent = (numItems + 1) + " items"
+function countItems(item, quantity) {
+    itemCar.textContent = (numItems + 1) + " items";
+    let encontrado = quantity.find(elementFood =>  elementFood.food == item.name);
+    if(encontrado === undefined)
+    {
+        let event = {};
+        event["food"] = item.name;
+        event["quantity"] = 1;
+        event["unitPrice"] = item.price;
+        event["amount"] = item.price;
+        quantity.push(event);
+        console.log(event);
+    } else {
+        encontrado.quantity ++;
+        encontrado.amount = encontrado.quantity * encontrado.unitPrice;
+    }
+    numItems = numItems + 1;
 }
 
+function addItems(item) {
+    a.push(item)
+}
